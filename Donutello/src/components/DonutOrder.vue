082d7b9b0130d1fac0933ddcad158681621e2025
ZienabAlr <r0784494@student.thomasmore.be>
2022-12-13 19:24:48 +0100

    feat(DonutOrder.vue): contact information form added

--- Donutello/src/components/DonutOrder.vue
+++ Donutello/src/components/DonutOrder.vue
@@ -4,7 +4,19 @@ import {onMounted, reactive, ref} from 'vue';
 </script>
 
 <template>
- 
+ <div>
+    <h1>Bestelling formulier</h1>
+    <div>
+        <label for="name">Bedrijfsnaam</label>
+        <input type="text" id="name" v-model="name">
+
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model="email">
+
+        <label for="phone">Telefoonnummer</label>
+        <input type="tel" id="phone" v-model="phone">
+    </div>
+ </div>  
 </template>
 
 <style scoped>
