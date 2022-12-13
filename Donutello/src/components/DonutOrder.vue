b7f844a094b6cf2a746a3b878248f8f20627dacb
ZienabAlr <r0784494@student.thomasmore.be>
2022-12-13 20:37:58 +0100

    fix(DonutOrder.js): labels and inputs are placed in divs

--- Donutello/src/components/DonutOrder.vue
+++ Donutello/src/components/DonutOrder.vue
@@ -4,72 +4,98 @@ import {onMounted, reactive, ref} from 'vue';
 </script>
 
 <template>
- <div>
+ <div class="bestellingForm">
     <h1>Bestelling formulier</h1>
-    <div>
+    <div class="formulier">
         <h2>Contact gegevens</h2>
-        <label for="name">Bedrijfsnaam</label>
-        <input type="text" id="name" v-model="name">
 
-        <label for="email">Email</label>
-        <input type="email" id="email" v-model="email">
-
-        <label for="phone">Telefoonnummer</label>
-        <input type="tel" id="phone" v-model="phone">
-    </div>
-    
-    <div>
-        <h2>Donut Bestelling</h2>
-        <label for="donut">Donut Naam</label>
-        <input type="text" id="donut" v-model="donut">
-
-        <label for="glaze">Glazuur</label>
-        <select id="glaze" v-model="donut">
-            <option disabled value="">Kies een glazuur</option>
-            <option>Chocolade</option>
-            <option>Melkchocolade</option>
-            <option>Witte Chocolade</option>
-            <option>Aardbei</option>
-
-        </select> 
-
-        <label for="toppings">Topping</label>
-        <select id="toppings" v-model="donut">
+        <div>
+            <label for="name">Bedrijfsnaam</label>
+            <input type="text" id="name" v-model="name">
+        </div>
+       
+        <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" v-model="email">
+        </div>
         
-            <option disabled value="">Kies een topping</option>
-            <option>Sprinkels Sweet Heaven</option>
-            <option>Sprinkels Red Nose</option>
-            <option>Oreo</option>
-            <option>Bueno</option>
+        <div>
+            <label for="phone">Telefoonnummer</label>
+            <input type="tel" id="phone" v-model="phone">
+        </div>
+        
+    </div>
 
-        </select> 
+    <div class="formulier">
+        <h2>Donut Bestelling</h2>
 
+        <div>
+            <label for="donut">Donut Naam</label>
+            <input type="text" id="donut" v-model="donut">
+
+        </div>
+        
+        <div>
+            <label for="glaze">Glazuur</label>
+            <select id="glaze" v-model="donut">
+                
+                <option disabled value="">Kies een glazuur</option>
+                <option>Chocolade</option>
+                <option>Melkchocolade</option>
+                <option>Witte Chocolade</option>
+                <option>Aardbei</option>
+
+            </select>
+        </div>
+
+        <div>
+            <label for="toppings">Topping</label>
+            <select id="toppings" v-model="donut">
+            
+                <option disabled value="">Kies een topping</option>
+                <option>Sprinkels Sweet Heaven</option>
+                <option>Sprinkels Red Nose</option>
+                <option>Oreo</option>
+                <option>Bueno</option>
+
+            </select> 
+        </div>
+      
+        <div>
+            <label for="logo">Logo</label>
+            <input type="file" id="file">
+        </div>
     
-        <label for="logo">Logo</label>
-        <input type="file" id="file">
+        <div>
+            <label for="logoshape">Logo Vorm</label>
+            <select id="logoshape" v-model="donut">
 
-        <label for="logoshape">Logo Vorm</label>
-        <select id="logoshape" v-model="donut">
+                <option disabled value="">Kies een vorm</option>
+                <option>Rechthoek</option>
+                <option>Cirkel</option>
+                <option>Ovaal</option>
+                <option>Driehoek</option>
+                <option>Vierkant</option>
 
-            <option disabled value="">Kies een vorm</option>
-            <option>Rechthoek</option>
-            <option>Cirkel</option>
-            <option>Ovaal</option>
-            <option>Driehoek</option>
-            <option>Vierkant</option>
+            </select>
+        </div>
 
-        </select>
-
-        <label for="amount">Aantal</label>
-        <input type="text" id="donut" v-model="donut">
-
-        <label for="note">Wil je nog iet zeggen?</label>
-        <textarea id="note" v-model="note"></textarea>
-
+        <div>
+            <label for="amount">Aantal</label>
+            <input type="text" id="donut" v-model="donut">
+        </div>
+        
+        <div>
+            <label for="note">Wil je nog iet zeggen?</label>
+            <textarea id="note" v-model="note"></textarea>
+        </div>
+        
     </div>
  </div>  
 </template>
 
 <style scoped>
+
+
 
 </style>
