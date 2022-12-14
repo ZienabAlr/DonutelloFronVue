4f4a0e07fd83ed1657e814bf770a304d930be085
ZienabAlr <r0784494@student.thomasmore.be>
2022-12-14 10:57:45 +0100

    feat(App.js): viewOrder component added

--- Donutello/src/App.vue
+++ Donutello/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import DonutOrder from './components/DonutOrder.vue';
+import ViewOrders from './components/ViewOrders.vue';
 </script>
 
 <template>
   <div class="app">
  
     <DonutOrder />
+    <ViewOrders />
   </div>
 </template>
 
